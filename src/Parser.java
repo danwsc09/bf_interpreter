cc195c22b0dcd3699df43adc532d4159f12541a2
Wonsang <sang.jeong@freedgrouptech.com>
2022-02-20 09:59:11 +0900

    parser (WIP); changing directory structure

--- src/Parser.java
+++ src/Parser.java
@@ -3,15 +3,41 @@ import java.util.List;
 import java.util.Stack;
 
 public class Parser {
-
-    enum Instruction {
+    enum InstructionCode {
         IncrementPointer,
         DecrementPointer,
         Increment,
         Decrement,
         Write,
         Read,
-        Loop
+        Loop,
+    }
+//    public static final int Comment = 0;
+//    public static final int IncrementPointer = 1;
+//    public static final int DecrementPointer = 2;
+//    public static final int Increment = 3;
+//    public static final int Decrement = 4;
+//    public static final int Write = 5;
+//    public static final int Read = 6;
+
+    class Instruction {
+        InstructionCode singleInstruction;
+        List<InstructionCode> multipleInstructions = new ArrayList<>();
+
+        public Instruction(Lexer.Token token) {
+            singleInstruction = switch (token) {
+                case RIGHT_ARROW -> InstructionCode.IncrementPointer;
+                case LEFT_ARROW -> InstructionCode.DecrementPointer;
+                case PLUS -> InstructionCode.Increment;
+                case MINUS -> InstructionCode.Decrement;
+                case PERIOD -> InstructionCode.Write;
+                case COMMA -> InstructionCode.Read;
+                default -> {
+                    yield Parser.Comment;
+                }
+            };
+        }
+
     }
 
     public static List<Instruction> parse(List<Lexer.Token> tokens) {
